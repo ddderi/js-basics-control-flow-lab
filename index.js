f058eb9cec007f6ac5eac5a2d6c699075620039c
ddderi <maxime830@live.fr>
2021-12-13 02:59:26 +0100

    commit tree 2319ce444713dc65e97d9004415a2362f473ba3c

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
+let feet = 199;
+
+
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+  return scuberGreetingForFeet = 'This one is on me!';
+} else if (feet > 2000 && feet <= 2500) { 
+  return scuberGreetingForFeet = 'I will gladly take your thirty bucks.';
+} else {  
+  return scuberGreetingForFeet = 'No can do.';
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+// switch(feet){
+//   case 'This is on me':
+//   feet  <= 400
+//   break;
+//   case 'I will gladly take your thirty bucks.':
+//   feet > 2000 
+//   break;
+//   case 'No can do.':
+//   feet > 2500
+//   break;
+// }
+ let city;
+
+function ternaryCheckCity(city){
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+let tip;
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case tip = 'generous':
+    return switchOnCharmFromTip = 'Thank you so much.'
+    break;
+    case tip = 'not as generous': 
+    return switchOnCharmFromTip = 'Thank you.'
+      break;
+    case tip = 'thanks for everything':
+    return switchOnCharmFromTip = "Bye."
+      break;
+  }
 }
